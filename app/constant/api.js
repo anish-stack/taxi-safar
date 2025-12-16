50b926428d989953b6d4828db68f7a55062eb63f
Jitender Kumar <jitenderkumar@Jitenders-MacBook-Air.local>
2025-12-17 00:47:39 +0530

    ok

--- app/constant/api.js
+++ app/constant/api.js
@@ -1,4 +1,4 @@
-export const API_URL_APP = 'http://192.168.1.10:3100'
-export const API_URL_APP_CHAT = 'http://192.168.1.10:3200'
+export const API_URL_APP = 'https://test.taxi.olyox.in'
+export const API_URL_APP_CHAT = 'https://chat.taxi.olyox.in/'
 
 export const api = 'AIzaSyBvyzqhO8Tq3SvpKLjW7I5RonYAtfOVIn8';
